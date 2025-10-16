39559d504a0214d0b49c6f0e6a62fe38f89d19ce
CaesarHD <stir.cezar@gmail.com>
2025-10-16 10:45:18 +0300

    Spring boot init function

--- src/main/java/org/ball/Main.java
+++ src/main/java/org/ball/Main.java
@@ -1,35 +1,31 @@
 package org.ball;
 
-import org.springframework.boot.CommandLineRunner;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
+import org.springframework.stereotype.Component;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 @SpringBootApplication
 public class Main {
+
+    private final PlayerRepository playerRepository;
+
+    public Main(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
-
     }
 
-    @Bean
-    CommandLineRunner run(PlayerRepository playerRepository, PlayerService playerService) {
-        return args -> {
-            playerRepository.save(new Player("Cezar"));
-            System.out.println(playerRepository.findAll());
-        };
+    @PostConstruct
+    public void init() {
+        playerRepository.save(new Player("Cezar"));
+        System.out.println(playerRepository.findAll());
     }
 }
